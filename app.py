162a6178c107a243358941b6cd30e91f0cc3e377
mandelaIan <78343118+mandelaIan@users.noreply.github.com>
2022-07-03 14:30:37 +0200

    Added PostgreSQL environment

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from resources.user import UserRegister
 from flask import Flask
 from flask_restful import Api
@@ -9,7 +10,7 @@ from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config['SQLAlCHEMY_TRACK_MODIFICATIONS'] = False 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.secret_key = 'jose'
 api = Api(app)
 
